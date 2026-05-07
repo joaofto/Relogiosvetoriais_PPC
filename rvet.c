9d239828bbf386b5d7de19449610ba0f6a000ca9
Otavio071 <177013ot@academico.ufs.br>
2026-05-07 17:11:50 +0000

    modificacao no process e na main para sincronizar os pontos

--- rvet.c
+++ rvet.c
@@ -69,9 +69,24 @@ void Receive(int src, Clock *clock) {
     int pid;
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     // TODO: receber vetor e atualizar clock local
-    
+     int received[3];
+
+    MPI_Recv(received, 3, MPI_INT, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    printf("P%d - Recebendo de P%d: (%d,%d,%d) - antes: (%d,%d,%d)\n", 
+           pid, src, received[0], received[1], received[2], 
+           clock->p[0], clock->p[1], clock->p[2]);
+    fflush(stdout);
+
+    for (int i = 0; i < 3; i++) {
+        if (received[i] > clock->p[i]) {
+            clock->p[i] = received[i];
+        }
+    }
+
+    clock->p[pid]++;
     // No final, imprime relógio atualizado
-    PrintClock("Recebimento de mensagem", clock);
+    PrintClock(label, clock);
 }
 
 /*
@@ -81,22 +96,44 @@ void Receive(int src, Clock *clock) {
 
 void process0() {
     Clock clock = {{0,0,0}};
-    PrintClock("Estado inicial", &clock);
+    InternalEvent(&clock, "a");                 // (1,0,0)
 
-    Event(&clock);
+    SendEvent(1, &clock, "b", TAG_B);           // (1,0,0)
+
+    ReceiveEvent(1, &clock, "c", TAG_H);        // recebe h de P1
+
+    InternalEvent(&clock, "d");                 // d
+
+    SendEvent(2, &clock, "d->m", TAG_DM);       // envia p/ P2
+
+    ReceiveEvent(2, &clock, "e", TAG_LE);       // recebe l->e de P2
+
+    InternalEvent(&clock, "f");                 // f
+
+    SendEvent(1, &clock, "f->j", TAG_FJ);       // envia p/ P1
+
+    InternalEvent(&clock, "g");                 // g
     // TODO: Send/Receive conforme diagrama
 }
 
 void process1() {
     Clock clock = {{0,0,0}};
-    PrintClock("Estado inicial", &clock);
-    // TODO
+    SendEvent(0, &clock, "h", TAG_H);           // (0,1,0) after send? NO - send doesn't increment
+
+    ReceiveEvent(0, &clock, "i", TAG_B);        // recebe b de P0
+
+    ReceiveEvent(0, &clock, "j", TAG_FJ);       // recebe f->j de P0
 }
 
 void process2() {
     Clock clock = {{0,0,0}};
-    PrintClock("Estado inicial", &clock);
-    // TODO
+   InternalEvent(&clock, "k");                 // k - (0,0,1)
+
+    InternalEvent(&clock, "l");                 // l - (0,0,2)
+
+    ReceiveEvent(0, &clock, "m", TAG_DM);       // recebe d->m de P0
+
+    SendEvent(0, &clock, "l->e", TAG_LE);       // envia p/ P0
 }
 
 int main(void) {
@@ -104,6 +141,11 @@ int main(void) {
 
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("Processo %d iniciado\n", my_rank);
+    fflush(stdout);
 
     if (my_rank == 0)
         process0();
